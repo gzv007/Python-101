c4cb6a12207951a99a819b0926d15a43c5b0fe73
gzv007 <rpg.668@gmail.com>
2022-06-15 14:48:35 +0700

    ลองสร้าง Repo ให้ python

---
+++ Beginner.py
@@ -0,0 +1,32 @@
+# # ทดสอบการแสดงผล
+# '''
+# ฝึกการ
+# เขียนโปรแกรม
+# '''
+# print("hello world")
+# print(1)
+# print(1.2)
+# print(True)
+# print(False)
+# print("Supamongkol\t" + "Meechu") #\t = tab
+
+#ตัวแปร
+#ชื่อตัวแปร = ค่าที่เก็บ
+x = 20 #int
+y = "20" #str
+z = True #boolean
+print("ยอดเงินคงเหลือ " + str(x) +" บาท") #แปลง int to str
+print(int(y) + 40) #แปลง str to int
+print(type(z)) #หาชนิดตัวแปร
+
+# #การตั้งชื่อตัวแปร
+# '''
+# 1.ห้ามขึ้นต้นด้วยตัวเลวหรือสัญลักษณ์พิเศษ
+# 2.ห้ามซ้ำกับ Keyword (คำสงวน)
+# 3.Case sensitive พิมพ์เล็กพิมพ์ใหญ่ มีความแตกต่าง
+# '''
+
+# รับข้อมูล input
+# name = input("กรุณาป้อนชื่อ : ")
+# age = input("กรุณาป้อนอายุ : ")
+# print("ชื่อของคุณคือ : " + str(name) + " อายุ : " + str(age))    
